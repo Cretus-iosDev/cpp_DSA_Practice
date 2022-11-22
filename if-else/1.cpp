af1b230d53e09ad406d01419b6f7a908195a0d18
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2022-11-22 10:13:43 +0530

    Add files via upload

---
+++ if-else/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int a=9;
+	if(a==9){
+		cout<<"NINEY"<<endl;
+	}
+	if(a>0){
+		cout<<"POSITIVE"<<endl;
+	}
+	else{
+		cout<<"NEGATIVE"<<endl;
+	}
+	return 0;
+}
---
+++ if-else/3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a =24;
+	if(a>20){
+		cout<<"rutik"<<endl;
+	}
+	else if(a==24){
+		cout<<"ramesh"<<endl;
+	}
+	else{
+		cout<<"maraskolhe"<<endl;
+	}
+	cout<<a;
+	return 0;
+}
