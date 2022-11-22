ab0d18fe7b3ee1e180178f58ac13465ae49e07cc
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2022-11-22 09:25:44 +0530

    Add files via upload

---
+++ Practice_code/basic_of_c++.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+/*
+  //dataTypes And Variable ->
+
+  //interger
+  int a =123;
+  cout<<a<<endl;
+
+  //character
+  char b='v';
+  cout<<b<<endl;
+
+  //boolean
+  bool b1=true;
+  cout<<b1<<endl;
+
+  //float
+  float f=1.2;
+  cout<<f<<endl;
+
+  //double
+  double d = 1.23;
+  cout<<d<<endl;
+
+  //sizeof(shows spaces)
+  int size = sizeof(d);
+  cout<<"size of a is:- "<<size<<endl;
+
+
+	//ASCII Value ->
+	int a ='{';
+	cout<<a<<'\n';
+
+  //typeCasting ->
+  char ch = 98;
+  cout<<ch<<'\n';
+
+  char ch1 = 123456;
+  cout<< ch1 << endl;
+
+
+  //How '-ve' Number are Store? ->
+  unsigned int a = -122;
+  cout<<a<<'\n';
+
+
+  //Operators ->
+  int a = 2.0/5;
+  cout<<a<<endl;
+
+
+  //Realtional Operator ->
+  int a = 2;
+  int b = 3;
+
+  bool first=(a==b);
+  cout<<first<<endl;
+
+   bool second=(a>b);
+  cout<<second<<endl;
+
+   bool third=(a<b);
+  cout<<third<<endl;
+
+   bool fourth=(a>=b);
+  cout<<fourth<<endl;
+
+   bool fifth=(a<=b);
+  cout<<fifth<<endl;
+
+   bool sixth=(a!=b);
+  cout<<sixth<<endl;
+
+
+  //Logical Operator
+  int a = 0;
+  cout<< !a <<endl;
+*/
+  return 0;
+}
